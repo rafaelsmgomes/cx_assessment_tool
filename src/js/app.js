2e06525a4cc5b9cb575aee567f18b4360a996dd3
Rafael Gomes <rafael.smgomes@gmail.com>
2019-11-18 14:53:05 -0800

    stabilished connection to DB

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
-var express = require('express'),
-    con     = require('mysql');
+const express = require('express'),
+      mysql   = require('mysql');
 
-var app = express();
+const app = express();
 
+const con = mysql.createConnection({
+    host: "72.10.48.193",
+    user: "root",
+    password: "DBase_0243",
+    database: "test_1"
+});
+
+con.query('CREATE TABLE question_schema ( question_id INT )', (err, results, fields) => {
+    if(err) throw err;
+    console.log(results);
+});
+
+con.end();

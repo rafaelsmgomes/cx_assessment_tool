e173d12829ef6818ef64bdb1483f516e8f632ac8
Mike Dacanay <migd64@gmail.com>
2020-01-06 13:05:11 -0800

    prevented stacked checkboxes from moving

--- src/js/views/panels.js
+++ src/js/views/panels.js
@@ -89,7 +89,6 @@ export const panels = [
 				'textArr': ['Barely', 'Some', 'A lot'],
 				'group': 'broadcast',
 				'count': true,
-				'valueArr': '[0, 66, 100]'
 			})}
 		</div>
 		<div class="btn__progress btn__progress--4" data-id='4' data-line='4'></div>

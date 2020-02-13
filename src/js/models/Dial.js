1d0404f01866f7108b38b67e7f2118e08420477a
Mike Dacanay <migd64@gmail.com>
2020-02-12 17:02:18 -0800

    added chosenAns to Dial Module

--- src/js/models/Dial.js
+++ src/js/models/Dial.js
@@ -6,9 +6,28 @@ export default class Dial {
     	this.textArr = attr.textArr;
     	this.group = attr.group; //Group its on
     	this.count = attr.count; //Whether it counts or not;
+        this.chosenAns = null;
     }
 
     assignValue(qLen,v){
     	this.val = Math.round(100*(v/180));    	
     }    
+
+    assignAns(v){
+        if(this.textArr.length === 3){
+            if(v <= 60){
+                this.chosenAns = this.textArr[0];
+            }else if(v > 60 && v <= 120){
+                this.chosenAns = this.textArr[1];
+            }else{
+                this.chosenAns = this.textArr[2];
+            }
+        }else{
+            if(v <= 90){
+                this.chosenAns = this.textArr[0];
+            }else{
+                this.chosenAns = this.textArr[1];
+            }
+        }
+    }
 }
